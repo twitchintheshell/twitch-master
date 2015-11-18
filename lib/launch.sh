2bb3ea5ff1dbaca256578b801a5b4a46eeae5d39
pie93 <computerwiznick@gmail.com>
2015-11-17 22:44:16 -0500

    Change to core2duo so we can support more os
    Change to core2duo so e can support more os' like mac :)

--- lib/launch.sh
+++ lib/launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #qemu-system-x86_64 -cdrom ~/archlinux-2015.10.01-dual.iso -hda ~/Arch.qcow2 -net nic,model=ne2k_pci -net user -m 2G -vnc localhost:0 -monitor stdio -localtime -qmp tcp:localhost:4444,server,nowait -no-shutdown
-qemu-system-x86_64 -cdrom ~/archlinux-2015.10.01-dual.iso -hda ~/Disk1.img -hdb ~/Disk2.img -net nic -net user -m 4G -vnc localhost:0 -monitor stdio -localtime -qmp tcp:localhost:4444,server,nowait -cpu host -smp 4 -no-shutdown -enable-kvm
+#qemu-system-x86_64 -cdrom ~/archlinux-2015.10.01-dual.iso -hda ~/Disk1.img -hdb ~/Disk2.img -net nic -net user -m 4G -vnc localhost:0 -monitor stdio -localtime -qmp tcp:localhost:4444,server,nowait -cpu host -smp 4 -no-shutdown -enable-kvm
+qemu-system-x86_64 -cdrom ~/archlinux-2015.10.01-dual.iso -hda ~/Disk1.img -hdb ~/Disk2.img -net nic -net user -m 4G -vnc localhost:0 -monitor stdio -localtime -qmp tcp:localhost:4444,server,nowait -cpu core2duo -smp 4 -no-shutdown -enable-kvm
